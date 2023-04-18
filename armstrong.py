050469def59b7eb10117d4a4383fe8b36fbb9e2d
GIRI-SUMANTH <girisumanth_uppalapati@srmap.edu.in>
2023-04-18 22:14:00 +0530

    Added Armstrong.py
    Armstrong Number Program

---
+++ armstrong.py
@@ -0,0 +1,17 @@
+x = int(input("Enter the number : "))
+k = x
+count = 0
+while (x != 0):
+    x = x//10
+    count = count+1
+sum, dig = 0, 0
+num = k
+while (k != 0):
+    dig = k % 10
+    sum = sum+(dig**count)
+    k = k//10
+#print(sum)
+if (sum == num):
+    print("\n Armstrong number.")
+else:
+    print("\n Not an Armstrong number.")
